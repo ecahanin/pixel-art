a62e92b6a651b3f1ceede5d8a8d0b995af0d378f
ecahanin <11097728+ecahanin@users.noreply.github.com>
2021-09-28 20:42:22 -0500

    added canvas

--- leaf_art.py
+++ leaf_art.py
@@ -1,5 +1,5 @@
 import numpy as np
-from random import choice
+from random import choice, randint
 
 from PIL import Image
 
@@ -16,8 +16,8 @@ CANVAS_SIZE = (300, 300)
 def get_leaf():
     leaf = Image.open('leaf.png')
     leaf.convert('RGBA')
-    leaf = color_leaf(leaf)
-    leaf.save('testleaf.png')
+    return color_leaf(leaf)
+    
     
 
 def color_leaf(img):
@@ -29,10 +29,28 @@ def color_leaf(img):
     colored_img = Image.fromarray(data, mode='RGBA')
     return colored_img
 
+def make_canvas(size=CANVAS_SIZE):
+    canvas = Image.new('RGBA', size, (255,255,255,0))
+    random_points = []
+    jitter = [-3,-2,-1,0,1,2,3]
+    for x in range(-10, size[0] + 10):
+        for y in range(-10, size[1] + 10):
+            xi = x + choice(jitter)
+            yi = y + choice(jitter)
+            if 0 <= xi < size[0]:
+                if 0 <= yi < size[1]:
+                    random_points.append((xi, yi))
+    while random_points:
+        coords = random_points.pop(randint(0,len(random_points)-1))
+        leaf = get_leaf()
+        canvas.paste(leaf, coords, leaf)
+    canvas.save('testc.png')
+
+        
 
 
 
 
 
 if __name__ == '__main__':
-    get_leaf()
+    make_canvas()
